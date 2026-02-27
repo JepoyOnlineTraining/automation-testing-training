fb6fed998d544e1c19a8f48d916c8f647f2c225d
Jeffrey <trainmyselfonline@gmail.com>
2026-02-27 20:55:52 +0800

    Adding Day3 files

---
+++ Day3/Test.ts
@@ -0,0 +1,28 @@
+
+
+
+let data:number = 10;
+
+// data = "10";
+
+console.log(data)
+
+
+let message = "Hello";
+
+let count = 30;
+
+console.log(`Type of message is ${typeof(message)}`)
+console.log(`Type of count is ${typeof(count)}`)
+
+let num1 = "5";
+let num2 = 3;
+
+console.log(`Output is ${num1 + num2}`);
+
+let num3:string = "5";
+let num4:number = 4;
+
+let result = num3+num4;
+
+console.log(result);
